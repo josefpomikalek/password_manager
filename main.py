6d5c3e1782d8fdcac37fa79c8d058f445cd43764
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-02 19:06:11 +0200

    Align the widths of buttons and entries.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=40, pady=40)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
@@ -18,18 +18,18 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
  
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
-password_entry = Entry(width=21)
+website_entry = Entry(width=51)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=51)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add")
-add_button.grid(row=4, column=1)
+add_button = Button(text="Add", width=43)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
